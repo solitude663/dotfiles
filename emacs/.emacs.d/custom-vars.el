b2c7cdb7c5c6f2b015891b86b160510a704b5149
Anthony Brown <brown.tonii663@gmail.com>
2026-04-15 08:30:51 -0500

    Updates

--- emacs/.emacs.d/custom-vars.el
+++ emacs/.emacs.d/custom-vars.el
@@ -7,8 +7,8 @@
  '(package-selected-packages
    '(company devdocs dumb-jump emmet-mode go-mode gruvbox-theme helpful
 	     hl-todo magit mood-line move-text multiple-cursors
-	     nasm-mode org-roam org-roam-ui typescript-mode vertico
-	     web-mode))
+	     nasm-mode org-roam org-roam-ui rust-mode typescript-mode
+	     vertico web-mode))
  '(warning-suppress-log-types '((treesit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
